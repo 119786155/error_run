6f19e43f7add5e54be7634a3d64e721c2dd4e0c0
119786155 <119786155@qq.com>
2025-09-28 03:41:01 +0800

    connect editor for writing

---
+++ src/routes/write/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Editor from './Editor.svelte';
+</script>
+
+<svelte:head>
+	<title>Write</title>
+	<meta name="description" content="write" />
+</svelte:head>
+
+<Editor />
---
+++ src/routes/write/Editor.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { registerRichText } from '@lexical/rich-text';
+  import { mergeRegister } from '@lexical/utils';
+  import { createEditor } from 'lexical'
+  import { onMount } from 'svelte';
+
+  const config = {
+    namespace: 'MyEditor',
+    onError: console.error
+  }
+
+  const editor = createEditor(config);
+
+  let myEditorRef;
+
+  onMount(() => {
+    editor.setRootElement(myEditorRef);
+
+    mergeRegister(
+      registerRichText(editor)
+    )
+  })
+
+</script>
+
+<div bind:this={myEditorRef} contenteditable></div>

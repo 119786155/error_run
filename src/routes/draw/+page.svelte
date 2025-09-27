ab2c143a189ff6b0645f285b23d9889932fe6ee9
119786155 <119786155@qq.com>
2025-09-28 02:11:03 +0800

    connect excalidraw

---
+++ src/routes/draw/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Draw from './Draw.svelte';
+  let excalidrawAPI = $state()
+
+  $effect(() => {
+    excalidrawAPI?.onChange(console.log)
+  })
+</script>
+
+<svelte:head>
+	<title>Draw</title>
+	<meta name="description" content="draw" />
+</svelte:head>
+
+<Draw excalidrawAPI={(api) => (excalidrawAPI = api)} />
---
+++ src/routes/draw/Draw.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { createElement } from 'react'
+	import { createRoot } from 'react-dom/client'
+  import "@excalidraw/excalidraw/index.css";
+
+  let props = $props()
+  let rootEl
+
+  $effect(() => {
+    const root = createRoot(rootEl)
+
+    import('@excalidraw/excalidraw').then(({ Excalidraw, WelcomeScreen, MainMenu }) => {
+      const welcome = createElement(WelcomeScreen, { key: 'WelcomeScreen' })
+      const menu = createElement(MainMenu, { key: 'MainMenu' }, [
+        createElement(MainMenu.DefaultItems.LoadScene, { key: 'LoadScene' }),
+        createElement(MainMenu.DefaultItems.SaveAsImage, { key: 'SaveAsImage' }),
+        createElement(MainMenu.DefaultItems.Export, { key: 'Export' }),
+        createElement(MainMenu.Separator, { key: 'Separator' }),
+        createElement(MainMenu.DefaultItems.ChangeCanvasBackground, {
+          key: 'ChangeCanvasBackground',
+        }),
+      ])
+      const excalidraw = createElement(Excalidraw, { ...props }, [welcome, menu])
+      root.render(excalidraw)
+    })
+
+    return () => root.unmount()
+  })
+</script>
+
+<div bind:this={rootEl} class="root"></div>
+
+<style>
+  .root {
+    height: 100svh;
+  }
+</style>

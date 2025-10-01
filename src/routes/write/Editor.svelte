1037e239e6ca485bf14155e2a979ce47d5449cc2
119786155 <119786155@qq.com>
2025-10-01 13:14:54 +0800

    connect svelte lexcial

--- src/routes/write/Editor.svelte
+++ src/routes/write/Editor.svelte
@@ -1,25 +1,28 @@
 <script>
-	import { registerRichText } from '@lexical/rich-text';
-	import { mergeRegister } from '@lexical/utils';
-	import { createEditor } from 'lexical';
-	import { onMount } from 'svelte';
+	import { Composer, ContentEditable, RichTextPlugin } from 'svelte-lexical';
+	import { theme } from 'svelte-lexical/dist/themes/default';
+	import Toolbar from './Toolbar.svelte';
 
-	const config = {
-		namespace: 'MyEditor',
-		onError: console.error
+	const initialConfig = {
+		theme: theme,
+		namespace: 'pg_demo',
+		nodes: [],
+		onError: (error) => {
+			throw error;
+		}
 	};
-
-	const editor = createEditor(config);
-
-	let myEditorRef;
-
-	onMount(() => {
-		editor.setRootElement(myEditorRef);
-
-		mergeRegister(registerRichText(editor));
-	});
 </script>
 
-<div class="editor container">
-	<div bind:this={myEditorRef} contenteditable></div>
-</div>
+<Composer {initialConfig}>
+	<div class="editor-shell svelte-lexical">
+		<Toolbar />
+		<div class="editor-container">
+			<div class="editor-scroller">
+				<div class="editor">
+					<ContentEditable />
+				</div>
+			</div>
+			<RichTextPlugin />
+		</div>
+	</div>
+</Composer>
---
+++ src/routes/write/Toolbar.svelte
@@ -0,0 +1,27 @@
+<script>
+	import {
+		BoldButton,
+		Divider,
+		ItalicButton,
+		UnderlineButton,
+		StrikethroughButton,
+		FormatCodeButton,
+		DropDownAlign,
+		FontFamilyDropDown,
+		FontSizeDropDown,
+		Toolbar
+	} from 'svelte-lexical';
+</script>
+
+<Toolbar>
+	<FontFamilyDropDown />
+	<FontSizeDropDown />
+	<Divider />
+	<BoldButton />
+	<ItalicButton />
+	<UnderlineButton />
+	<StrikethroughButton />
+	<FormatCodeButton />
+	<Divider />
+	<DropDownAlign />
+</Toolbar>

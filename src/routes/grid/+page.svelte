ca9dc60b7329e105f62754d130623020eb3d450d
119786155 <119786155@qq.com>
2025-10-18 20:37:04 +0800

    connect grid

---
+++ src/routes/grid/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Grid from './Grid.svelte';
+</script>
+
+<svelte:head>
+	<title>Grid</title>
+	<meta name="description" content="grid" />
+</svelte:head>
+
+<Grid />
---
+++ src/routes/grid/Grid.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { RevoGrid } from '@revolist/svelte-datagrid';
+
+	const source = [
+		{
+			name: '1',
+			details: 'Item 1'
+		},
+		{
+			name: '2',
+			details: 'Item 2'
+		}
+	];
+
+	const columns = [
+		{
+			prop: 'name',
+			name: 'First',
+			cellTemplate(h, { value }) {
+				return h('span', { style: { background: 'red' } }, value);
+			}
+		},
+		{
+			prop: 'details',
+			name: 'Second'
+		}
+	];
+</script>
+
+<RevoGrid {source} {columns} />

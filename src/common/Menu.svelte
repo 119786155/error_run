6c4fc7f57f9455004c094ee5e9c3d19c817c557c
119786155 <119786155@qq.com>
2025-10-26 23:37:20 +0800

    style of menu

--- src/common/Menu.svelte
+++ src/common/Menu.svelte
@@ -3,7 +3,7 @@
 	import { page } from '$app/state';
 </script>
 
-<nav class="glass-card">
+<nav class="menu-container glass-card">
 	<ul class="menu-list">
 		<li
 			aria-current={page.url.pathname === '/' || page.url.pathname === '/story'
@@ -22,7 +22,7 @@
 </nav>
 
 <style>
-	nav {
+	.menu-container {
 		width: 15rem;
 		height: 15rem;
 		border-radius: 50%;
@@ -37,7 +37,7 @@
 		z-index: 10;
 	}
 
-	nav:hover {
+	.menu-container:hover {
 		left: -9rem;
 	}
 
@@ -50,7 +50,7 @@
 		transition: var(--transition);
 	}
 
-	nav:hover .menu-list {
+	.menu-container:hover .menu-list {
 		display: block;
 	}
 
@@ -74,42 +74,5 @@
 	.menu-list li[aria-current='page'] a {
 		color: var(--text-color);
 		opacity: 0.5;
-	}
-
-	.glass-card {
-		background: rgba(255, 255, 255, 0.05);
-		backdrop-filter: blur(5px);
-		border: 1px solid rgba(255, 255, 255, 0.3);
-		box-shadow:
-			0 8px 32px rgba(0, 0, 0, 0.1),
-			inset 0 1px 0 rgba(255, 255, 255, 0.5),
-			inset 0 -1px 0 rgba(255, 255, 255, 0.1),
-			inset 0 0 0px 0px rgba(255, 255, 255, 0);
-		overflow: hidden;
-	}
-
-	.glass-card::before {
-		content: '';
-		position: absolute;
-		top: 0;
-		left: 0;
-		right: 0;
-		height: 1px;
-		background: linear-gradient(90deg, transparent, rgba(255, 255, 255, 0.8), transparent);
-	}
-
-	.glass-card::after {
-		content: '';
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 1px;
-		height: 100%;
-		background: linear-gradient(
-			180deg,
-			rgba(255, 255, 255, 0.8),
-			transparent,
-			rgba(255, 255, 255, 0.3)
-		);
 	}
 </style>
